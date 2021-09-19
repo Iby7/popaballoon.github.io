d604509d5626f878dea48237db6cb2bc03854bca
Muhammad Ibrahim <iby.khan7@gmail.com>
2021-09-19 13:15:35 +0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCjMwuX9vuSYmXZCJe_hsYvczJq3xlR4Zk",
+    authDomain: "pop-a-balloon-f00c3.firebaseapp.com",
+    projectId: "pop-a-balloon-f00c3",
+    storageBucket: "pop-a-balloon-f00c3.appspot.com",
+    messagingSenderId: "80513115202",
+    appId: "1:80513115202:web:7d00cbcd56642186cfb9db",
+    measurementId: "G-1BZDXC2RHX"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
